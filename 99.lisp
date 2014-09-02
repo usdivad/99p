077d5757d0549d617e49da0038fb9e58146a3d93
David Su <david.d.su@gmail.com>
2014-09-02 00:26:42 -0400

    solve 2

--- 99.lisp
+++ 99.lisp
@@ -2,25 +2,36 @@
 ;BUT A LISP AIN'T ONE
 
 ;1
+;8/?
 (defun my-last (l)
 	(cond
-		((null l) nil)
+		((null l) '())
 		((null (cdr l)) (car l))
 		(t (my-last (cdr l)))
 	))
-(print (cons 'bacon (my-last '(2 4 5 22 'bacon))))
+;(print (cons 'bacon (my-last '(2 4 5 22 'bacon))))
+(print (my-last '(egg and cheese)))
 
 ;2
-(defun show-var ()
-	(let ((var 123)) var))
-(print (show-var))
+;9/1: 0018-0025
+
+;var declaration unnecessary
+; (defun show-var ()
+; 	(let ((var 123)) var))
+; (print (show-var))
 
 (defun my-but-last (l)
-	(let ((last (my-last l)))
 		(cond
-			((null l) nil)
+			((null l) '())
 			;( ( eq ((car (cdr l)) last) ) (car l)) ;problem part
-			((null (cdr l)) (car l))
+			((null (cdr (cdr l))) (car l))
 			(t (my-but-last (cdr l)))
-		)))
-(print (my-but-last '(1 2 3 'bacon 4)))
+		)
+	)
+(print (my-but-last '(1 2 3 bacon 4)))
+
+;3
+;0025-
+(defun element-at (l k)
+	(t l)
+	)
